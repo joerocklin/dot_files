3b7cfe3a7a61b637a2d7bcadfb086ef3a029b9ab
Joe Rocklin <joe.rocklin@gmail.com>
2015-02-27 11:36:43 -0500

    Removing everything unnecessary for repo manifests

--- README.md
+++
@@ -1,6 +0,0 @@
-dot_files
-=========
-
-Some of my config files.
-
-I manage them with rcm: http://robots.thoughtbot.com/rcm-for-rc-files-in-dotfiles-repos
--- rcm-install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://thoughtbot.github.io/rcm/dist/rcm-1.2.3.tar.gz && \
-tar -xvf rcm-1.2.3.tar.gz && \
-cd rcm-1.2.3 && \
-
-./configure --prefix=/home/${USER} && \
-make && \
-make install
